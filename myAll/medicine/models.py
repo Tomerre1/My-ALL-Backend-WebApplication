cfe52a9e71123f141203a060a0adcacffc84a645
raziel_perez <razielp18@gmail.com>
2021-12-16 12:30:55 +0200

    update model medicine

--- myAll/medicine/models.py
+++ myAll/medicine/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Medicine(models.Model):
     medicineName=models.CharField(max_length=100,primary_key=True)
-    description=models.CharField(max_length=300)
+    description=models.CharField(max_length=1500)
     level=models.CharField(max_length=100,null=True,default=None)
     count=models.CharField(max_length=100,null=True,default=None)
-    badInfluence=models.CharField(max_length=100,null=True,default=None)
+    badInfluence=models.CharField(max_length=1000,null=True,default=None)
     foodOrNot=models.CharField(max_length=100,null=True,default=None)
 
 
