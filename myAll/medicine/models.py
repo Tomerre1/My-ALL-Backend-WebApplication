6db6d5498dc64c6d95619bf80a6392b9b7db62fa
raziel_perez <razielp18@gmail.com>
2021-12-29 21:58:35 +0200

    update medicine model

--- myAll/medicine/models.py
+++ myAll/medicine/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.db.models import JSONField
+
 
 class Medicine(models.Model):
     medicineName=models.CharField(max_length=100,primary_key=True)
     description=models.CharField(max_length=1500)
-    level=models.CharField(max_length=100,null=True,default=None)
+    level=JSONField(default=list, blank=True, null=True)
     count=models.CharField(max_length=100,null=True,default=None)
     badInfluence=models.CharField(max_length=1000,null=True,default=None)
     foodOrNot=models.CharField(max_length=100,null=True,default=None)
