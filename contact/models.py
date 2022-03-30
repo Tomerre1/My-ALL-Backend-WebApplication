d108c54ebe4abfc939b0105beaf8298a4ceb69f4
raziel_perez <razielp18@gmail.com>
2022-03-30 12:50:18 +0300

    change contact app

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     phone =models.CharField(max_length=15)
     mail = models.EmailField(max_length=100)
     job =models.CharField(max_length=100)
-    imgUrl=models.URLField( max_length=1500)
+    img=models.URLField( max_length=1500)
 
     def __str__(self):
         return 'Name:{0} ,job: {1},phone: {2}'.format(self.name, self.job, self.phone)
