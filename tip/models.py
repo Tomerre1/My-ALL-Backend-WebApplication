7351ec33d7346e589ce0463eea4ce63f2e620a55
raziel_perez <razielp18@gmail.com>
2022-03-30 12:17:14 +0300

    update tip app

--- tip/models.py
+++ tip/models.py
@@ -7,7 +7,7 @@ class Tip(models.Model):
     title =models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     date =models.CharField(max_length=100)
-    label=models.CharField(max_length=100)
+    label=models.JSONField(default=list, blank=True, null=True)
 
     def __str__(self):
         return 'id:{0} ,title: {1},content: {2}'.format(self.id, self.title, self.content)
